67a7bca58328055d3d9f6fc78cc2abf00b0d627b
ptorrsmith-javln <peter.torr.smith@javln.com>
2026-01-03 03:30:03 +0000

    switch off Ruby’s output buffering so plays well with compose

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,7 @@
+# By default, Ruby buffers output to stdout, which doesn’t play well with Compose.
+#  Let’s fix this by switching off Ruby’s output buffering.
+$stdout.sync = true
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.

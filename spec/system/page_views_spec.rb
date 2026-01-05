19788d819195d754489bcd130021107b67933fdd
Peter Torr Smith <peter.torr.smith@fluxfederation.com>
2026-01-05 13:37:11 +1300

    system spec comment re intel vs ARM silicon

--- spec/system/page_views_spec.rb
+++ spec/system/page_views_spec.rb
@@ -6,8 +6,12 @@ RSpec.describe 'PageViews' do
     expect(page.text).to match(/This page has been viewed [0-9]+ times?!/)
   end
 
+  # this doesn't work with rack_test driver as no JS support
+  # and only runs on intel, not on apple silicon
+  # so commented out for now
   xit 'is enhanced with JavaScript on', js: true do
     visit '/welcome'
+    # sleep 10 # for viewing in the browser during test runs
     expect(page).to have_text('ENHANCED!')
   end
 end
